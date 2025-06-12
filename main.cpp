88f14cc68f29ca7ebb17121909b35a93ed7f08d6
Nate Hartway <nah0905@gmail.com>
2025-06-12 11:13:58 -0500

    Created Group Project Project

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//Author(s): Nate Hartway
+//Purpose:
+//Date:
+//Version:
+
+#include <iostream>
+
+int main() {
+
+    auto lang = "C++";
+    std::cout << "Hello and welcome to " << lang << "!\n";
+
+    return 0;
+    
+}
